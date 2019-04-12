4d51d13400ece0230ef22d48a032fdd06d5a7bcb
Yermek Kadyrbayev <user@mail.com>
2019-04-12 17:26:22 +0300

    checkstyle issue fixed

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -1,7 +1,7 @@
-import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
 
 public class EmployeeTest {
 
